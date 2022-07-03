3348191896374419085d915675d2b9b2c278c562
ashrafsherwani <96438004+ashrafsherwani@users.noreply.github.com>
2022-07-03 22:12:03 +0300

    Add files via upload

---
+++ aurus.js
@@ -0,0 +1,19 @@
+/* Start function onclick event button*/
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  /* End function for more and less button*/
+
